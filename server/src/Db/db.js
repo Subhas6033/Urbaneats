de3de28e555a840d224e5411c4d2946c80ea093b
Subhas <goalkeepersubhas07@gmail.com>
2025-09-06 00:59:18 +0530

    Update the mongodb uri console message

--- server/src/Db/db.js
+++ server/src/Db/db.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const connectionInstances = await mongoose.connect(
+    await mongoose.connect(
       `${process.env.MONGODB_URI}/${process.env.DB_NAME}/?retryWrites=true&w=majority&appName=Cluster0`
     );
-    console.log(
-      'Successfully connected to the DB !!!',
-      connectionInstances.connection._connectionString
-    );
+    console.log('Successfully connected to the DB !!!');
   } catch (error) {
     console.log(`ERR while connecting to the DB !!!`, error);
   }
