df440c1c431046450155f0da37ccfc8e1cb4e183
Subhas <goalkeepersubhas07@gmail.com>
2025-09-06 00:20:07 +0530

    create the server and add DB in the app

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import { app } from './app.js';
+import dotenv from 'dotenv';
+import { connectDB } from './src/Db/db.js';
+
+dotenv.config({
+  path: '../.env',
+});
+
+connectDB()
+  .then(() => {
+    app.get('/', (req, res) => {
+      res.send('Welcome to Server');
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is running on Port : http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log('ERR while running the Server', err);
+  });
--- server/src/Db/db.js
+++ server/src/Db/db.js
@@ -1,3 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export const connectDB = mongoose.connect(``)
+export const connectDB = async () => {
+  try {
+    const connectionInstances = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}//?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log(
+      'Successfully connected to the DB !!!',
+      connectionInstances.connection._connectionString
+    );
+  } catch (error) {
+    console.log(`ERR while connecting to the DB !!!`, error);
+  }
+};
